c436ade6fd1c6027226f186e9ba9d03d6ebd18f2
rogadde <rkgadde@ucsd.edu>
2022-07-13 20:02:01 -0700

    Add comments to rmdup script

--- workflow/scripts/slavseq_rmdup_hts.pl
+++ workflow/scripts/slavseq_rmdup_hts.pl
@@ -39,21 +39,22 @@ sub remove_tmp_and_return_to_old_dir
 
 sub prio_pair_rmdup
 {
-    my ($fn, $ofn)=@_;
-    my $bam = Bio::DB::HTSfile->open($fn);
+    my ($fn, $ofn)=@_; # store arguments as variables
+    my $bam = Bio::DB::HTSfile->open($fn); # read input bam file
   
     #my $good_bam = Bio::DB::HTSfile->open($good_ofn, 'w');
     #my $bad_bam = Bio::DB::HTSfile->open($bad_ofn, 'w');
 
     #assume reads are sorted by name
     # my $header = $bam->header(); # Bio:DB:Sam
-    my $header = $bam->header_read;
+    my $header = $bam->header_read; # read header info
  
     #$good_bam->header_write($header);
     #$bad_bam->header_write($header);
     
-    my $target_names = $header->target_name;
+    my $target_names = $header->target_name; # return a reference to an array of reference sequence names
 
+	# write to second argument
     open OUT, " | sort -S 5000M -k 4,4 -k 2,2rn -k 3,3rn | uniq -f 3 -c | perl -pe 's/^ +(\\d+) +(\\S+)/\$2\\tXD:i:\$1/' | cut -f 1,2 | sort -S 5000M > $ofn";
 
     #open OUT, " > $ofn";
@@ -61,17 +62,17 @@ sub prio_pair_rmdup
     my $r1=undef;
     my $r2=undef;
     
-    while($r2 = $bam->read1($header))
+    while($r2 = $bam->read1($header)) # read one alignment from file and return Alignment object
     {
 	# filter out secondary matches (bwa mem compatibility)
 	# secondary matches won't be used in selection
-	next if ($r2->flag & 256) || ($r2->flag & 2048); 
+	next if ($r2->flag & 256) || ($r2->flag & 2048); # continue to next iteration if true; flag is SAM bitwise flag field
 	
 
 	if(defined($r1))
 	{
 	    if(
-		($r2->qname ne $r1->qname) ||
+		($r2->qname ne $r1->qname) || # if read names are not the same...
 		(($r1->flag & 4) && ($r1->flag & 64)) # drop if R1 unmapped
 		)
 		
@@ -84,6 +85,7 @@ sub prio_pair_rmdup
 
 		if($r1->flag & 128)
 		{
+			# switch values of r1 and r2
 		    my $tmp=$r1;
 		    $r1=$r2;
 		    $r2=$tmp;
@@ -92,17 +94,20 @@ sub prio_pair_rmdup
 		my $sumqual=0;
 		for my $q ($r1->qscore, $r2->qscore)
 		{
-		    $sumqual+=$q;
+		    $sumqual+=$q; # sum  of quality scores
 		}
 		
+		# pos is 0-based leftmost coordinate of the aligned sequence on the reference sequence, calend is rightmost
 		my $r1pos=($r1->flag & 16)?$r1->calend+1:$r1->pos+1;
 		my $r1strand=($r1->flag & 16)?'-':'+';
 		
 		#my $r2pos=($r2->flag & 4)?'*':($r2->flag & 16)?$r2->calend+1:$r2->pos+1;
 		#my $r2strand=($r2->flag & 4)?'*':($r2->flag & 16)?'-':'+';
 		
+		# join target ID, position, and strand info
 		my $pos=join(':',$target_names->[$r1->tid], $r1pos, $r1strand);
 		
+		# join and print more metrics to output file
 		print OUT join("\t", $r1->qname, $r1->qual + $r2->qual, $sumqual, $pos),"\n";
 		
 		$r2=undef;
@@ -130,7 +135,8 @@ chdir $tmpdir;
 
 prio_pair_rmdup 'input.bam', 'selected.txt';
 
-system("samtools view -H input.bam > header.txt");
+system("samtools view -H input.bam > header.txt"); # get header only
+# sort input bam by first column, output lines identical to selected.txt, and append header to create output bam file 
 system(qq[samtools view input.bam | sort -T ./ -S 1500M -s -k 1,1 | join -t "\t" - selected.txt | cat header.txt - | samtools view -S -b - > output.bam]);
 chdir $curdir;
 rename "$tmpdir/output.bam", "$output_bam_fn";
