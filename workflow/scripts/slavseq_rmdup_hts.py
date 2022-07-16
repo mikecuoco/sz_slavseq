173919007b3c89b94b885931460bb03c02d0f196
rogadde <rkgadde@ucsd.edu>
2022-07-15 19:59:39 -0700

    Preliminary script (without tmpdirs)

--- workflow/scripts/slavseq_rmdup_hts.py
+++ workflow/scripts/slavseq_rmdup_hts.py
@@ -1,30 +1,34 @@
 import pysam
 import subprocess
+import sys
+import os
+import locale
 
-def prio_pair_rmdup(bam_in, txt):
-    bam = pysam.AlignmentFile(bam_in, "rb") # read input bam file
-    outfile = open(txt, "w") # open output file for writing
+# def use_tmp_dir(path):
+#     # TODO
+#     tmpbase = path
+#     old = path
+
+def prio_pair_rmdup(filename, out_filename):
+    bam = pysam.AlignmentFile(filename, "rb") # read input bam file
+    outfile = open(out_filename, "w+") # open output file for writing
 
     r1 = None
     r2 = None
 
     while r2 = bam.fetch(until_eof = True):
         
-        # filter out secondary matches (bwa mem compatibility)
+        # filter out secondary (and supplementary) matches (bwa mem compatibility)
 	    # secondary matches won't be used in selection
-	    # 256 = not primary alignment
-	    # 2048 = supplementary alignment
-	    # see https://broadinstitute.github.io/picard/explain-flags.html for SAM flags info
-        if (r2.flag & 256) or (r2.flag & 2048):
+        if (r2.is_secondary) or (r2.is_supplementary):
             continue # continue to next iteration if true
         
         else if r1 != None:
-            if (r2.qname != r1.qname) or ((r1.flag & 4) and (r1.flag & 64)):
-                # if read names are not the same or 
-                # if R1 is unmapped (4 = unmapped, 64 = first in pair), 
+            if (r2.qname != r1.qname) or ((r1.is_unmapped) and (r1.is_read1)):
+                # if read names are not the same or if R1 is unmapped, 
                 # do nothing
             else: # this is a good pair
-                if r1.flag & 128: # 128 = second in pair
+                if r1.is_read2:
                     # switch values of r1 and r2
                     tmp = r1
                     r1 = r2
@@ -38,34 +42,56 @@ def prio_pair_rmdup(bam_in, txt):
                 r1pos = r1.query_alignment_start + 1 # convert from 0-based to 1-based coordinates
                 
                 # label strand
-                if r1.flag & 16:
-                    r1strand = "-"
-                else:
-                    r1strand = "+"
+                r1strand = "-" if r1.is_reverse else "+"
 
                 # join target ID, position, and strand info
                 pos = ":".join([r1.reference_name, r1pos, r1strand])
                 # join and print more metrics to output file
                 all_fields = "\t".join([r1.qname, r1.mapping_quality + r2.mapping_quality, sumqual, pos]) + "\n"
 
-                p1 = subprocess.Popen(["sort", "-S", "5000M", "-k", "4,4", "-k", "2,2rn", "-k", "3,3rn"], stdout=subprocess.PIPE)
-                p1_out = p1.communicate(all_fields.encode())[0]
-                p2 = subprocess.Popen(["uniq", "-f", "3", "-c"], stdin=p1_out, stdout=subprocess.PIPE)
-                p1.stdout.close()
-
-                p3 = subprocess.Popen(["perl", "-pe", 's/^ +(\\d+) +(\\S+)/\$2\\tXD:i:\$1/'], stdin=p2.stdout, stdout=subprocess.PIPE)
-                p2.stdout.close()
-
-                p4 = subprocess.Popen(["cut", "f", "1,2"], stdin=p3.stdout, stdout=subprocess.PIPE)
-                p3.stdout.close()
-
-                p5 = subprocess.run(["sort", "S", "5000M"], stdin=p4.stdout, stdout=outfile, text = True)
-                p4.stdout.close()
+                subprocess.run(
+                    "sort -S 5000M -k 4,4 -k 2,2rn -k 3,3rn - | uniq -f 3 -c |" +
+                    "perl -pe 's/^ +(\\d+) +(\\S+)/\$2\\tXD:i:\$1/' | cut -f 1,2 | sort -S 5000M", 
+                    input=all_fields, stdout=outfile, shell=True, check=True, text=True)
 
                 r2 = None
         
         r1 = r2
 
-    bam.close()
+    outfile.close()
 
-prio_pair_rmdup("input.bam", "selected.txt")
+input_bam_fn = sys.argv[1]
+output_bam_fn = sys.argv[2]
+
+# input_bam_path = os.path.abspath(sys.argv[1])
+
+if os.path.exists(output_bam_fn):
+    sys.exit("Output file already exists!")
+
+locale.setlocale(locale.LC_ALL, "C")
+
+# pwd = os.getcwd().strip()
+# curdir, tmpdir = use_tmp_dir(pwd)
+# os.symlink(input_bam_path, tmpdir + "/input.bam")
+# os.chdir(tmpdir)
+
+prio_pair_rmdup(
+    "results/" + input_bam_fn, 
+    "results/" + input_bam_fn.replace(".bam", "_") + "selected.txt")
+
+input_bam = open("results/" + input_bam_fn, "r")
+header = open("results/" + input_bam_fn.replace(".bam", "_") + "header.txt", "w+")
+output_bam = open("results" + output_bam_fn, "w+")
+
+subprocess.run(["samtools", "view", "-H", "-"], stdin=input_bam, stdout=header, check=True, text=True)
+subprocess.run(
+    "samtools view - | sort -T ./ -S 1500M -s -k 1,1 |" +
+    "join -t '\t' - results/selected.txt | cat results/header.txt - | samtools view -S -b -",
+    stdin=input_bam, stdout=output_bam, shell=True, check=True)
+
+input_bam.close()
+header.close()
+output_bam.close()
+
+os.remove("results/" + input_bam_fn.replace(".bam", "_") + "selected.txt")
+os.remove("results/" + input_bam_fn.replace(".bam", "_") + "header.txt")
