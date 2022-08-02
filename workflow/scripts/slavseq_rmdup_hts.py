82558e0e8efcd134d61379a2568e2282ba013c48
rogadde <rkgadde@ucsd.edu>
2022-08-02 14:02:12 -0700

    Update rmdup script

--- workflow/scripts/slavseq_rmdup_hts.py
+++ workflow/scripts/slavseq_rmdup_hts.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
+__author__ = "Rohini Gadde"
 
 import pysam
 import subprocess
 import sys
 import os
-import locale
-
-# def use_tmp_dir(path):
-#     # TODO
-#     tmpbase = path
-#     old = path
+import argparse
+import tempfile
+import shutil
+import glob
+from pathlib import Path
 
 def prio_pair_rmdup(filename, out_filename):
     bam = pysam.AlignmentFile(filename, "rb") # read input bam file
@@ -42,62 +42,93 @@ def prio_pair_rmdup(filename, out_filename):
                 for q in query_qualities:
                     sumqual += q
                 
-                r1pos = r1.query_alignment_start + 1 # convert from 0-based to 1-based coordinates
+                r1pos = r1.reference_end + 1 if r1.is_reverse else r1.reference_start + 1 # convert from 0-based to 1-based coordinates
                 
                 # label strand
                 r1strand = "-" if r1.is_reverse else "+"
 
                 # join target ID, position, and strand info
                 pos = ":".join([r1.reference_name, str(r1pos), r1strand])
+                
                 # join and print more metrics to output file
                 all_fields = "\t".join([r1.qname, str(r1.mapping_quality + r2.mapping_quality), str(sumqual), pos])
                 
-                subprocess.run(
-                    "sort -S 5000M -k 4,4 -k 2,2rn -k 3,3rn | uniq -f 3 -c |" +
-                    "perl -pe 's/^ +(\\d+) +(\\S+)/\$2\\tXD:i:\$1/' | cut -f 1,2 | sort -S 5000M", 
-                    input=all_fields, stdout=outfile, shell=True, check=True, text=True)
+                outfile.write(all_fields + "\n")
 
                 r2 = None
         
         r1 = r2
 
     outfile.close()
+    
+def parse_args():
 
-input_bam_fn = sys.argv[1]
-output_bam_fn = sys.argv[2]
+    parser = argparse.ArgumentParser(description="Remove duplicates from BAM files")
+    parser.add_argument(
+        "-b", "--bam", type=Path, required=True, help="input BAM file"
+    )
+    parser.add_argument(
+        "-o", "--out", type=Path, required=True, help="output BAM file"
+    )
+    
+    args = parser.parse_args()
 
-# input_bam_path = os.path.abspath(sys.argv[1])
+    return args
 
-if os.path.exists(output_bam_fn):
-    sys.exit("Output file already exists!")
+def main():
+    
+    # get arguments
+    args = parse_args()
 
-locale.setlocale(locale.LC_ALL, "C")
+    input_bam_fn = args.bam
+    output_bam_fn = args.out
 
-# pwd = os.getcwd().strip()
-# curdir, tmpdir = use_tmp_dir(pwd)
-# os.symlink(input_bam_path, tmpdir + "/input.bam")
-# os.chdir(tmpdir)
+    if os.path.exists(output_bam_fn):
+        sys.exit("Output file already exists!")
 
-prio_pair_rmdup(
-    input_bam_fn, 
-    "results/selected.txt")
+    os.environ['LC_ALL'] = "C"
 
-input_bam = open(input_bam_fn, "r")
-header = open("results/header.txt", "w+")
-output_bam = open(output_bam_fn, "w+")
+    curdir = os.getcwd()
+    tmpdir = tempfile.mkdtemp(dir="./")
+    os.symlink(input_bam_fn, tmpdir + "/input.bam")
+    os.chdir(tmpdir)
 
-subprocess.run(["samtools", "view", "-H"], stdin=input_bam, stdout=header, check=True, text=True)
+    prio_pair_rmdup(
+        "input.bam", 
+        "all_fields.txt")
+    
+    unsorted_ids = open("all_fields.txt", "r")
+    sorted_ids = open("selected.txt", "w+")
 
-input_bam.seek(0) # reset file pointer to start of file
-p1 = subprocess.Popen(["samtools", "view"], stdin=input_bam, stdout=subprocess.PIPE, text=True)
-p2 = subprocess.run(
-    "sort -T ./ -S 1500M -s -k 1,1 |" +
-    "join -t '\t' - results/selected.txt | cat results/header.txt - | samtools view -S -b -",
-    stdin=p1.stdout, stdout=output_bam, shell=True, check=True, text=True)
+    # NOTE: shell=True can lead to security vulnerabilities
+    subprocess.run(
+        'sort -S 5000M -k 4,4 -k 2,2rn -k 3,3rn | uniq -f 3 -c | ' +
+        'perl -pe "s/^ +(\\d+) +(\\S+)/\$2\\tXD:i:\$1/" | cut -f 1,2 | sort -S 5000M',
+        stdin=unsorted_ids, stdout=sorted_ids, shell=True, check=True, text=True)
+    
+    input_bam = open("input.bam", "r")
+    header = open("header.txt", "w+")
+    output_bam = open("output.bam", "w+")
 
-input_bam.close()
-header.close()
-output_bam.close()
+    subprocess.run(["samtools", "view", "-H"], stdin=input_bam, stdout=header, check=True)
 
-os.remove("results/selected.txt")
-os.remove("results/header.txt")
+    input_bam.seek(0) # reset file pointer to start of file
+    p1 = subprocess.Popen(["samtools", "view"], stdin=input_bam, stdout=subprocess.PIPE)
+    p2 = subprocess.run(
+        'sort -T ./ -S 1500M -s -k 1,1 | ' +
+        'join -t "\t" - selected.txt | cat header.txt - | samtools view -S -b -',
+        stdin=p1.stdout, stdout=output_bam, shell=True, check=True)
+
+    input_bam.close()
+    header.close()
+    output_bam.close()
+
+    os.chdir(curdir)
+    shutil.move(tmpdir + "/output.bam", output_bam_fn)
+
+    return tmpdir
+
+if __name__ == "__main__":
+    dirname = main()
+    
+    shutil.rmtree(dirname)
