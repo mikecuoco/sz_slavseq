ea3f21d2bc91f71fecf0e80f4baf8661786fd9dd
mikecuoco <mcuoco@ucsd.edu>
2022-09-28 13:37:14 -0700

    remove unused pkgs/scripts, add nodefaults channel

--- workflow/scripts/bed_to_vcf.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-args = commandArgs(trailingOnly=TRUE)
-
-library(bedr)
-
-# header is chr start end
-x <- read.table(args[1], header=TRUE)
-bed2vcf(x, filename=args[2], zero.based=TRUE, header=NULL, fasta=args[3])
