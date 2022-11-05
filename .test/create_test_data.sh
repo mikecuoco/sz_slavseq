4b2065c9b242dff75448c9716eeff53a6de96b42
mikecuoco <mcuoco@ucsd.edu>
2022-11-05 10:34:10 -0700

    new test data, using chr21 and chr22

--- .test/create_test_data.sh
+++ .test/create_test_data.sh
@@ -11,13 +11,21 @@
 # out_prefix = prefix for the output files (including outdir)
 # region = region of the genome in format (chrN:start-end)
 
-if [[ $# -lt 4 ]] || [[ $# -gt 5 ]]; then 
+export LC_ALL=C
+
+if [[ $# -lt 4 ]]; then 
     echo "Usage: ./create_test_data.sh <bam> <fastq_R1> <fastq_R2> <out_prefix> <region>." \
         "First four arguments necessary!"
     exit 1
 fi
 
-export LC_ALL=C
+# put regions into an array
+if [[ $# -gt 4 ]]; then
+    regions=()
+    for n in $(seq 5 $#); do
+        regions+=("${!n}")
+    done
+fi
 
 if [[ "$2" != *R1*.fastq* ]] || [[ "$3" != *R2*.fastq* ]]; then
     echo "FASTQs incorrectly named"
@@ -27,10 +35,11 @@ fi
 if [[ $# -eq 4 ]]; then
     samtools view "$1" | awk '{print $1}' > tmp_ids.txt
 else
-    samtools view "$1" "$5" | awk '{print $1}' > tmp_ids.txt
+    samtools view "$1" "${regions[@]}" | awk '{print $1}' > tmp_ids.txt
 fi
 
 zcat "$2" | fgrep -f tmp_ids.txt -A 3 --no-group-separator - > "$4_R1.fastq"
 zcat "$3" | fgrep -f tmp_ids.txt -A 3 --no-group-separator - > "$4_R2.fastq"
+gzip $4_R*.fastq
 
 rm tmp_ids.txt
