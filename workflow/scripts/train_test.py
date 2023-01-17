554f280a26a957d339287b75225689de9659015e
mikecuoco <mcuoco@ucsd.edu>
2023-01-17 15:39:59 -0800

    add feature PCA, tSNE, cm, and feature importance

--- workflow/scripts/train_test.py
+++ workflow/scripts/train_test.py
@@ -9,7 +9,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.utils import shuffle
 from sklearn.metrics import precision_recall_curve, auc, confusion_matrix
-
+import pdb
 
 def make_pipeline(clf_type, params):
 
@@ -18,46 +18,64 @@ def make_pipeline(clf_type, params):
         from sklearn.ensemble import RandomForestClassifier
 
         clf = RandomForestClassifier()
-        pipe = Pipeline([(clf_type, clf)])
+
     elif clf_type == "LogisticRegression":
         from sklearn.linear_model import LogisticRegression
 
         clf = LogisticRegression()
-        pipe = Pipeline([("scaler", StandardScaler()), (clf_type, clf)])
+        pipe = [("scaler", StandardScaler())]
+
     elif clf_type == "SVC":
         from sklearn.svm import SVC
 
         clf = SVC()
-        pipe = Pipeline([("scaler", StandardScaler()), (clf_type, clf)])
+        pipe = [("scaler", StandardScaler())]
+
     elif clf_type == "MLPClassifier":
         from sklearn.neural_network import MLPClassifier
 
         clf = MLPClassifier()
-        pipe = Pipeline([(clf_type, clf)])
+
     elif clf_type == "CascadeForestClassifier":
         from deepforest import CascadeForestClassifier
 
         clf = CascadeForestClassifier()
-        pipe = Pipeline([(clf_type, clf)])
+
     elif clf_type == "DecisionTreeClassifier":
         from sklearn.tree import DecisionTreeClassifier
 
         clf = DecisionTreeClassifier()
-        pipe = Pipeline([(clf_type, clf)])
+
     elif clf_type == "GradientBoostingClassifier":
         from sklearn.ensemble import GradientBoostingClassifier
 
         clf = GradientBoostingClassifier()
-        pipe = Pipeline([(clf_type, clf)])
+
     elif clf_type == "HistGradientBoostingClassifier":
         from sklearn.ensemble import HistGradientBoostingClassifier
 
         clf = HistGradientBoostingClassifier()
-        pipe = Pipeline([(clf_type, clf)])
+    
+    elif clf_type == "AdaBoostClassifier":
+        from sklearn.ensemble import AdaBoostClassifier
+
+        clf = AdaBoostClassifier()
+
     elif clf_type == "XGBClassifier":
         from xgboost import XGBClassifier
 
         clf = XGBClassifier()
+
+    elif clf_type == "KNeighborsClassifier":
+        from sklearn.neighbors import KNeighborsClassifier
+
+        clf = KNeighborsClassifier()
+        pipe = [("scaler", StandardScaler())]
+
+    if "pipe" in locals():
+        pipe.append((clf_type, clf))
+        pipe = Pipeline(pipe)
+    else:
         pipe = Pipeline([(clf_type, clf)])
 
     # add user-specified hyperparameters
@@ -92,6 +110,10 @@ if __name__ == "__main__":
         pipe = make_pipeline(snakemake.params.model_name, snakemake.params.model_params)
         pipe.fit(features[fold]["train"], y_true[fold]["train"])
 
+        # save feature importances
+        if hasattr(pipe._final_estimator, 'feature_importances_') and hasattr(pipe._final_estimator, 'feature_names_in_'):
+            metrics[fold]['feature_importances'] = pd.DataFrame({"feature name": pipe._final_estimator.feature_names_in_, "importance": pipe._final_estimator.feature_importances_}).set_index("feature name")
+            
         # make shuffled test data
         y_true[fold]["test_shuffled"] = shuffle(y_true[fold]["test"], random_state=42)
 
@@ -106,16 +128,8 @@ if __name__ == "__main__":
                 y_pred[fold][stage] = pipe.predict(features[fold][stage])
                 y_proba[fold][stage] = pipe.predict_proba(features[fold][stage])
 
-            # get confusion matrices
-            # raw
+            # get confusion matrix
             metrics[fold][stage]["cm"] = confusion_matrix(
-                y_true=le.inverse_transform(y_true[fold][stage]),
-                y_pred=le.inverse_transform(y_pred[fold][stage]),
-                labels=le.classes_,
-            )
-
-            # normalized
-            metrics[fold][stage]["cm_norm"] = confusion_matrix(
                 y_true=le.inverse_transform(y_true[fold][stage]),
                 y_pred=le.inverse_transform(y_pred[fold][stage]),
                 labels=le.classes_,
