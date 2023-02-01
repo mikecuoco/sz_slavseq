348e9abc2ec807123b9498085d7f793b3adaaba1
mikecuoco <mcuoco@ucsd.edu>
2023-02-01 11:35:14 -0800

    call peaks for non-ref insertions

--- workflow/rules/peaks.smk
+++ workflow/rules/peaks.smk
@@ -3,13 +3,11 @@ rule peaks:
         bam=rules.bwa_mem.output.bam,
         bai=rules.bwa_mem.output.index,
     output:
-        "{outdir}/results/peaks/{ref}/{donor}/{dna_type}/{sample}_{t}.bed",
+        "{outdir}/results/peaks/{ref}/{donor}/{dna_type}/{sample}.bed",
     conda:
         "../envs/peaks.yml"
-    params:
-        cluster_threshold=lambda wc: int(wc.t),  # must be >=1 or None
     log:
-        "{outdir}/results/peaks/{ref}/{donor}/{dna_type}/{sample}_{t}.log",
+        "{outdir}/results/peaks/{ref}/{donor}/{dna_type}/{sample}.log",
     script:
         "../scripts/peaks.py"
 
@@ -20,34 +18,13 @@ rule peaks_report:
         rl1=rules.run_rmsk.output,
         knrgl=rules.get_donor_knrgl.output,
     output:
-        "{outdir}/results/peaks/{ref}/{donor}/{dna_type}/{sample}_cluster{t}.ipynb",
+        "{outdir}/results/peaks/{ref}/{donor}/{dna_type}/{sample}.ipynb",
     conda:
         "../envs/peaks.yml"
     log:
-        notebook="{outdir}/results/peaks/{ref}/{donor}/{dna_type}/{sample}_cluster{t}.ipynb",
+        notebook="{outdir}/results/peaks/{ref}/{donor}/{dna_type}/{sample}.ipynb",
     notebook:
         "../notebooks/peaks_report.py.ipynb"
-
-
-rule compare_peaks_report:
-    input:
-        peaks=expand(
-            rules.peaks.output,
-            t=[0, 1, 2, 4, 8, 16, 32, 64, 128, 256],
-            allow_missing=True,
-        ),
-        rl1=rules.run_rmsk.output,
-        knrgl=rules.get_donor_knrgl.output,
-    output:
-        "{outdir}/results/peaks/{ref}/{donor}/{dna_type}/{sample}_compare.ipynb",
-    conda:
-        "../envs/peaks.yml"
-    params:
-        t=[0, 1, 2, 4, 8, 16, 32, 64, 128, 256],
-    log:
-        notebook="{outdir}/results/peaks/{ref}/{donor}/{dna_type}/{sample}_compare.ipynb",
-    notebook:
-        "../notebooks/compare_peaks_report.py.ipynb"
 
 
 rule render_peaks_report:
--- workflow/scripts/peaks.py
+++ workflow/scripts/peaks.py
@@ -1,92 +1,57 @@
 import sys, pysam
 import pandas as pd
-import numpy as np
-from scipy.spatial.distance import pdist, squareform
 
 
 sys.stderr = open(snakemake.log[0], "w")
 
-# # iterate over reads
+# iterate over reads
 bam = pysam.AlignmentFile(snakemake.input.bam, "rb")  # must be coordinate sorted
-peaks = {}
-print("Finding L1 reads with same start position...")
+peaks = {}; 
+print("Calling non-reference L1 insertions...")
 for r in bam.fetch():
-    # ignore marked duplicates
-    if r.is_duplicate:
+
+    # # only use primary alignments
+    # if r.is_secondary or r.is_supplementary or r.is_unmapped:
+    #     continue
+
+    # ignore unmapped reads
+    if r.is_unmapped:
         continue
 
-    # only use primary alignments
-    if r.is_secondary or r.is_supplementary:
-        continue
-
-    # ignore reads on edges of chrs
-    if r.reference_end is None or r.reference_start is None:
-        continue
-
-    if r.reference_name not in peaks.keys():
-        peaks[r.reference_name] = {}
-
-    # TODO: check if reads have been clipped
-    # TODO: add mate information when adding read2 to peak
-    if r.is_read2:  # get read that targets L1
-
-        # get start of read
-        if r.is_reverse:
-            coord = r.reference_end
-        else:
-            coord = r.reference_start
-
-        # create/add to peak
-        if coord in peaks[r.reference_name].keys():
-            peaks[r.reference_name][coord].append(r)
-        else:
-            peaks[r.reference_name][coord] = [r]
+    # investigate discordant read1
+    if r.is_read1:
+        if r.template_length == 0 or abs(r.template_length) > 2000:
+            if r.reference_name not in peaks.keys(): # create new list for each chr
+                peaks[r.reference_name] = []
+                i, start, end = 0, 0, 0
+                peaks[r.reference_name].append([r])
+            elif r.get_overlap(start, end) > 0: # if overlaps last read, add to existing peak
+                peaks[r.reference_name][i].append(r)
+            else: # create new peak
+                i += 1
+                peaks[r.reference_name].append([r])
+            start = r.reference_end if r.is_reverse else r.reference_start
+            end = r.reference_start if r.is_reverse else r.reference_end
 
 
-# cluster peaks
-print("Clustering pileups...")
-t = snakemake.params.cluster_threshold
-dist = {}
-new_peaks = {}
-for chr in peaks.keys():
-
-    # skip chr if no peaks
-    chr_coords = list(peaks[chr].keys())
-    if chr_coords == []:
-        continue
-
-    # get distance matrix
-    m = np.array([coord for coord in chr_coords]).reshape(-1, 1)
-    dist[chr] = squareform(pdist(m, "euclidean"))
-
-    # cluster peaks
-    i = 0
-    new_peaks[chr] = {}  # initialize
-    while i < len(chr_coords):
-        # get all peaks within t bp
-        (k,) = np.where(dist[chr][i] <= t)
-        coords = [chr_coords[j] for j in k]
-        start, end = min(coords), max(coords)
-        end += 1 if start == end else 0  # add 1 to end if start == end
-        new_peaks[chr][(start, end)] = [r for c in coords for r in peaks[chr][c]]
-        i = max(k) + 1
+import ipdb; ipdb.set_trace()
 
 
 # convert to bed file
 print("Saving peaks to BED...")
-cols = {"chr": [], "start": [], "end": [], "num_reads": []}
-for chr in new_peaks.keys():
-    for start, end in new_peaks[chr].keys():
+bed = {"chr": [], "start": [], "end": [], "num_reads": []}
+for chr in peaks.keys():
+    for p in peaks[chr]:
         # skip peaks with only one read
-        if len(new_peaks[chr][(start, end)]) == 1:
+        if len(p) == 1:
             continue
 
-        cols["chr"].append(chr)
-        cols["start"].append(start)
-        cols["end"].append(end)
-        cols["num_reads"].append(len(new_peaks[chr][(start, end)]))
+        bed["chr"].append(chr)
+        bed["start"].append(min([r.reference_end if r.is_reverse else r.reference_start for r in p]))
+        bed["end"].append(max([r.reference_start if r.is_reverse else r.reference_end for r in p]))
+        bed["num_reads"].append(len(p))
 
 # save to file
-pd.DataFrame(cols).to_csv(snakemake.output[0], sep="\t", index=False, header=False)
+pd.DataFrame(bed).to_csv(snakemake.output[0], sep="\t", index=False, header=False)
 
 sys.stderr.close()
