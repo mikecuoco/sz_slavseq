49c2c3ef3ad319d627d156785041115f97936511
mikecuoco <mcuoco@ucsd.edu>
2023-02-01 17:18:27 -0800

    black format

--- workflow/scripts/peaks.py
+++ workflow/scripts/peaks.py
@@ -19,15 +19,21 @@ for r in bam.fetch():
     # 2. is discordant (template length <= 0, or > 2000)
     # 3. has clipped bases (S or H in MC (mate-cigar) tag)
     if r.is_read1:
-        if not r.is_proper_pair or r.template_length <= 0 or r.template_length > 2000 or "S" in r.get_tag("MC") or "H" in r.get_tag("MC"):
+        if (
+            not r.is_proper_pair
+            or r.template_length <= 0
+            or r.template_length > 2000
+            or "S" in r.get_tag("MC")
+            or "H" in r.get_tag("MC")
+        ):
 
             # if overlap with previous peak, add to peak
-            if r.reference_name in peaks.keys() and r.get_overlap(start, end) > 0: 
+            if r.reference_name in peaks.keys() and r.get_overlap(start, end) > 0:
                 peaks[r.reference_name][-1].append(r)
                 if r.is_reverse:
                     start = r.reference_end if r.reference_end < start else start
                     end = r.reference_start if r.reference_start > end else end
-                else: 
+                else:
                     start = r.reference_start if r.reference_start < start else start
                     end = r.reference_end if r.reference_end > end else end
                 continue
@@ -35,7 +41,7 @@ for r in bam.fetch():
             # create new list for each chr
             if r.reference_name not in peaks.keys():
                 peaks[r.reference_name] = []
-                
+
             # if no overlap, create new peak
             peaks[r.reference_name].append([r])
             start = r.reference_end if r.is_reverse else r.reference_start
@@ -43,18 +49,38 @@ for r in bam.fetch():
 
 # convert to bed file
 print("Saving peaks to BED...")
-bed = {"chr": [], "start": [], "end": [], "r1_count": [], "r1_uniq_starts": [], "r1_uniq_ends": [], "r1_primary": [], "r1_supplementary": []}
+bed = {
+    "chr": [],
+    "start": [],
+    "end": [],
+    "r1_count": [],
+    "r1_uniq_starts": [],
+    "r1_uniq_ends": [],
+    "r1_primary": [],
+    "r1_supplementary": [],
+}
 for chr in peaks.keys():
     for p in peaks[chr]:
         # skip peaks with only one read
         if len(p) == 1:
             continue
 
-        features = {"r1_start": np.array([], dtype=int), "r1_end": np.array([], dtype=int), "r1_primary": 0, "r1_supplementary": 0}
+        features = {
+            "r1_start": np.array([], dtype=int),
+            "r1_end": np.array([], dtype=int),
+            "r1_primary": 0,
+            "r1_supplementary": 0,
+        }
         for r in p:
-            features["r1_start"] = np.append(features["r1_start"], r.reference_end if r.is_reverse else r.reference_start)
-            features["r1_end"] = np.append(features["r1_end"], r.reference_start if r.is_reverse else r.reference_end)
-            
+            features["r1_start"] = np.append(
+                features["r1_start"],
+                r.reference_end if r.is_reverse else r.reference_start,
+            )
+            features["r1_end"] = np.append(
+                features["r1_end"],
+                r.reference_start if r.is_reverse else r.reference_end,
+            )
+
             if r.is_supplementary:
                 features["r1_supplementary"] += 1
             else:
