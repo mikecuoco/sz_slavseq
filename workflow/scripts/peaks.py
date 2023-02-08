ad0ff427fade396ccbde43f4e53e67accc3b7837
mikecuoco <mcuoco@ucsd.edu>
2023-02-08 13:08:51 -0800

    make peaks from read1 YA, YG, and MAPQ

--- workflow/scripts/peaks.py
+++ workflow/scripts/peaks.py
@@ -1,7 +1,6 @@
 import sys, pysam
 import pandas as pd
 import numpy as np
-from itertools import groupby
 
 sys.stderr = open(snakemake.log[0], "w")
 
@@ -16,54 +15,63 @@ for r in bam.fetch():
         continue
 
     # make peaks from read1 if
-    # TODO: use thresholds for # of clipped bases in read2
-    # 1. is not a proper pair
-    # 2. is discordant (template length <= 0, or > 2000)
-    # 3. has clipped bases (S or H in MC (mate-cigar) tag)
-    if r.is_read1:
-        if (
-            r.template_length <= 0
-            or r.template_length > 2000
-            or "S" in r.get_tag("MC")
-            or "H" in r.get_tag("MC")
-        ):
+    # 1. is read1
+    # 2. has YA (R2 L1 alignment score) and YG (R2 reference genome alignment score) tags
+    # 3. YA > YG
+    if (
+        r.is_read1
+        and r.has_tag("YA")
+        and r.has_tag("YG")
+        and r.get_tag("YA") > r.get_tag("YG") # ignore reference L1 insertions
+        and r.get_tag("YA") > 20
+        and not r.has_tag("SA")
+        and not r.has_tag("XA")
+        and r.mapping_quality >= 60
+    ):
+        
 
-            # if overlap with previous peak, add to peak
-            if r.reference_name in peaks.keys() and r.get_overlap(start, end) > 0:
-                peaks[r.reference_name][-1].append(r)
-                if r.is_reverse:
-                    start = r.reference_end if r.reference_end < start else start
-                    end = r.reference_start if r.reference_start > end else end
-                else:
-                    start = r.reference_start if r.reference_start < start else start
-                    end = r.reference_end if r.reference_end > end else end
+        # if overlap with previous peak, add to peak
+        if (r.reference_name in peaks.keys()) and (r.get_overlap(start, end) > 10):
+
+            # skip duplicates
+            if r.is_reverse and (r.reference_end == start) and (r.reference_start == end):
+                continue
+            elif (not r.is_reverse) and (r.reference_start == start) and (r.reference_end == end):
                 continue
 
-            # create new list for each chr
-            if r.reference_name not in peaks.keys():
-                peaks[r.reference_name] = []
 
-            # if no overlap, create new peak
-            peaks[r.reference_name].append([r])
-            start = r.reference_end if r.is_reverse else r.reference_start
-            end = r.reference_start if r.is_reverse else r.reference_end
+            peaks[r.reference_name][-1].append(r)
+            if r.is_reverse:
+                start = r.reference_end if r.reference_end < start else start
+                end = r.reference_start if r.reference_start > end else end
+            else:
+                start = r.reference_start if r.reference_start < start else start
+                end = r.reference_end if r.reference_end > end else end
+            continue
+
+        # create new list for each chr
+        if r.reference_name not in peaks.keys():
+            peaks[r.reference_name] = []
+
+        # create new peak
+        peaks[r.reference_name].append([r])
+        start = r.reference_end if r.is_reverse else r.reference_start
+        end = r.reference_start if r.is_reverse else r.reference_end
+bam.close()
 
 # convert to bed file
-print("Saving peaks to BED...")
+print("Collecting peak features...")
+# TODO: iterate over peaks and collect features from all reads in each peak
 bed = {
     "chr": [],
     "start": [],
     "end": [],
-    "r1_count": [],
-    "r1_uniq_starts": [],
-    "r1_uniq_ends": [],
-    "r1_primary": [],
-    "r1_secondary": [],
-    "r1_supplementary": [],
-    "median_r2_poly_A_length": [],
-    "median_r2_ref_score": [],
-    "median_r2_L1_score": [],
+    "width": [],
+    "total_reads": [],
 }
+
+# reopen bam file
+bam = pysam.AlignmentFile(snakemake.input.bam, "rb")  # must be coordinate sorted
 for chr in peaks.keys():
     for p in peaks[chr]:
         # skip peaks with only one read
@@ -73,12 +81,6 @@ for chr in peaks.keys():
         features = {
             "r1_start": np.array([], dtype=int),
             "r1_end": np.array([], dtype=int),
-            "r1_primary": 0,
-            "r1_secondary": 0,
-            "r1_supplementary": 0,
-            "r2_poly_A_length": np.array([], dtype=int),
-            "r2_ref_score": np.array([], dtype=int),
-            "r2_L1_score": np.array([], dtype=int),
         }
         for r in p:
             features["r1_start"] = np.append(
@@ -90,45 +92,24 @@ for chr in peaks.keys():
                 r.reference_start if r.is_reverse else r.reference_end,
             )
 
-            if r.is_supplementary:
-                features["r1_supplementary"] += 1
-            elif r.is_secondary:
-                features["r1_secondary"] += 1
-            else:
-                features["r1_primary"] += 1
-
-            # r2_poly_A_length
-            if r.has_tag("Y2"):
-                y2 = groupby(r.get_tag("Y2"))
-                pA = max([sum(1 for _ in g) for l, g in y2 if l == "A"]) if "A" in r.get_tag("Y2") else 0
-                features["r2_poly_A_length"] = np.append(features["r2_poly_A_length"], pA)
-
-            # r2_ref_score
-            if r.has_tag("YG"):
-                features["r2_ref_score"] = np.append(
-                    features["r2_ref_score"], r.get_tag("YG")
-                )
-
-            # r2_L1_score
-            if r.has_tag("YA"):
-                features["r2_L1_score"] = np.append(
-                    features["r2_L1_score"], r.get_tag("YA")
-                )
-
         bed["chr"].append(chr)
         bed["start"].append(features["r1_start"].min())
         bed["end"].append(features["r1_end"].max())
-        bed["r1_count"].append(len(p))
-        bed["r1_uniq_starts"].append(len(np.unique(features["r1_start"])))
-        bed["r1_uniq_ends"].append(len(np.unique(features["r1_end"])))
-        bed["r1_primary"].append(features["r1_primary"])
-        bed["r1_secondary"].append(features["r1_secondary"])
-        bed["r1_supplementary"].append(features["r1_supplementary"])
-        bed["median_r2_poly_A_length"].append(np.median(features["r2_poly_A_length"]))
-        bed["median_r2_ref_score"].append(np.median(features["r2_ref_score"]))
-        bed["median_r2_L1_score"].append(np.median(features["r2_L1_score"]))
+        bed["width"].append(bed["end"][-1] - bed["start"][-1])
+        bed["total_reads"].append(len(p))
+
+        # # find peak summit
+        # covr = np.zeros(bed["width"][-1])
+        # for c in bam.count_coverage(chr, bed["start"][-1], bed["end"][-1]):
+        #     covr = np.add(covr,c)
+        # summit = np.where(covr == max(covr))
+        # import pdb; pdb.set_trace()
+        # bed["start"][-1] = bed["start"][-1] + summit[0][0]
+        # bed["end"][-1] = bed["start"][-1] + summit[0][-1]
+        # bed["width"][-1] = bed["end"][-1] - bed["start"][-1]
 
 # save to file
-pd.DataFrame(bed).to_csv(snakemake.output[0], sep="\t", index=False, header=False)
+print("saving to file...")
+pd.DataFrame(bed).to_csv(snakemake.output[0], index=False, sep="\t", header=None)
 
 sys.stderr.close()
