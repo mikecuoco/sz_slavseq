9ead2a3cbb132e58b3bc43f0c363457aad6a000c
mikecuoco <mcuoco@ucsd.edu>
2023-05-31 11:06:00 -0700

    cleanup tag_reads.py

--- workflow/scripts/tag_reads.py
+++ workflow/scripts/tag_reads.py
@@ -9,7 +9,9 @@ sys.stderr = open(snakemake.log[0], "w")
 pysam.set_verbosity(0)
 
 # initialize variables
-r1, r2 = None, None
+r1, r2 = type("", (), {})(), type("", (), {})()
+r1.query_name, r2.query_name = "", None
+assert r1.query_name != r2.query_name
 
 # iterate over read IDs in line1_bam, match with genome_bam reads
 # save primary r1 from genome, primary r2 from genome, and primary r2 from line-1 to tab-delimited file
@@ -25,15 +27,13 @@ with pysam.AlignmentFile(snakemake.input["genome_bam"], "rb") as genome_bam:
                     continue
 
                 # find matching reads in genome_bam
-                while True:
+                while (r1.query_name != r2.query_name) or (
+                    r1.query_name != r_l1.query_name
+                ):
                     r_genome = next(genome_bam)
 
-                    # skip secondary and supplementary alignments
+                    # get each read's primary alignment
                     if r_genome.is_secondary or r_genome.is_supplementary:
-                        continue
-
-                    # skip if genome read ID does not match line1 read ID
-                    if r_genome.query_name != r_l1.query_name:
                         continue
 
                     # get the read, save as r1 or r2
@@ -42,25 +42,23 @@ with pysam.AlignmentFile(snakemake.input["genome_bam"], "rb") as genome_bam:
                     else:
                         r2 = r_genome
 
-                    # skip if read1 or read2 is None
-                    if r1 is None or r2 is None:
-                        continue
-
-                    # break loop when both read1 and read2 are found
-                    elif r1.query_name == r2.query_name:
-                        break
-
-                # skip if read1 is a duplicate
-                if r1.is_duplicate or (r1.is_unmapped and r2.is_unmapped):
+                # skip if read1 is a duplicate or unmapped
+                if r1.is_duplicate or r1.is_unmapped:
                     continue
+
+                assert (
+                    r1.query_name == r2.query_name == r_l1.query_name
+                ), "Read IDs do not match"
 
                 # add tags to read1
                 r1.set_tag("ML", r_l1.get_tag("AS"))
                 r1.set_tag("MG", r2.get_tag("AS"))
                 r1.set_tag("MS", r_l1.reference_start)
+                r1.set_tag("ME", r_l1.reference_end)
                 r1.set_tag("MA", r_l1.query_sequence.count("A"))
 
-                # write read1 to output bam
+                # write reads to output bam
                 out_bam.write(r1)
+                out_bam.write(r2)
 
 sys.stderr.close()
