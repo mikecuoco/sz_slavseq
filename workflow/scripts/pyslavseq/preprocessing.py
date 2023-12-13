86770391fd1ec4a6c9ba7eb5a8ec7c072299ead2
mikecuoco <mcuoco@ucsd.edu>
2023-12-13 09:12:54 -0800

    update genome stratifications function

--- workflow/scripts/pyslavseq/preprocessing.py
+++ workflow/scripts/pyslavseq/preprocessing.py
@@ -2,6 +2,9 @@
 # Created on: Aug 8, 2023 at 1:53:30 PM
 __author__ = "Michael Cuoco"
 
+# source: Genome In A Bottle (GIAB) genome stratifications
+# https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/
+
 # configure logging
 import logging
 
@@ -9,33 +12,38 @@ logger = logging.getLogger(__name__)
 
 import pandas as pd
 
+STRATIFICATIONS = {
+    "chm13": {
+        "nonunique": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/v3.3/CHM13@all/Mappability/CHM13_nonunique_l100_m2_e1.bed.gz",
+        "difficult": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/v3.3/validation/CHM13@all/Union_CHM13_alldifficultregions.bed.gz",
+        "benchmark": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/data/AshkenazimTrio/analysis/NIST_HG002_DraftBenchmark_defrabbV0.012-20231107/CHM13v2.0_HG002-T2TQ100-V1.0_stvar.benchmark.bed",
+    },
+    "grch38": {
+        "nonunique": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/v3.3/GRCh38@all/Mappability/GRCh38_nonunique_l100_m2_e1.bed.gz",
+        "difficult": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/v3.3/GRCh38@all/Union/GRCh38_alldifficultregions.bed.gz",
+        "benchmark": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/data/AshkenazimTrio/analysis/NIST_HG002_DraftBenchmark_defrabbV0.012-20231107/GRCh38_HG002-T2TQ100-V1.0_stvar.benchmark.bed",
+    },
+}
 
-def get_hg38_blacklist() -> pd.DataFrame:
-    """
-    Get blacklist MHC, KIR, Tandem Repeat, SegDups, Gaps, and False duplicated regions from NCBI for hg38 genome builds
-    """
-    # read blacklist regions from NCBI
-    region_urls = {
-        "mhc": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/v3.0/GRCh38/OtherDifficult/GRCh38_MHC.bed.gz",
-        "kir": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/v3.0/GRCh38/OtherDifficult/GRCh38_KIR.bed.gz",
-        "trs": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/v3.0/GRCh38/LowComplexity/GRCh38_AllTandemRepeats_201to10000bp_slop5.bed.gz",
-        "segdups": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/v3.0/GRCh38/SegmentalDuplications/GRCh38_segdups.bed.gz",
-        "gaps": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/v3.0/GRCh38/OtherDifficult/GRCh38_gaps_slop15kb.bed.gz",
-        "false_dup": "https://ftp-trace.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/genome-stratifications/v3.0/GRCh38/OtherDifficult/GRCh38_false_duplications_correct_copy.bed.gz",
-    }
 
-    # add to dictionary
-    regions = []
-    for id, url in region_urls.items():
-        regions.append(
-            pd.read_csv(
-                url,
-                sep="\t",
-                header=None,
-                skiprows=1,
-                names=["Chromosome", "Start", "End"],
-            )
+def get_stratification(genome: str, name: str) -> pd.DataFrame:
+    """
+    Get specified genome stratification for grch38 or chm13 from GIAB FTP
+    """
+
+    if genome not in STRATIFICATIONS:
+        raise Exception(
+            f"Invalid genome {genome}, must be one of {list(STRATIFICATIONS.keys())}"
         )
-        regions[-1]["Name"] = id
+    if name not in STRATIFICATIONS[genome]:
+        raise Exception(
+            f"Invalid stratification {name}, must be one of {list(STRATIFICATIONS[genome].keys())}"
+        )
 
-    return pd.concat(regions)
+    return pd.read_csv(
+        STRATIFICATIONS[genome][name],
+        sep="\t",
+        header=None,
+        skiprows=1,
+        names=["Chromosome", "Start", "End"],
+    )
