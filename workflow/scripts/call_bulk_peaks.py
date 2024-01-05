7fafc9c8d408b58462dc07af17d8d7b3614a70eb
mikecuoco <mcuoco@ucsd.edu>
2024-01-05 10:02:42 -0800

    update bulk peak calling for new sliding_window

--- workflow/scripts/call_bulk_peaks.py
+++ workflow/scripts/call_bulk_peaks.py
@@ -14,39 +14,24 @@ logger = logging.getLogger(__name__)
 
 from pysam import AlignmentFile
 from pyslavseq.sliding_window import SlidingWindow
-import pyarrow.parquet as pq
 import pyranges as pr
+import pandas as pd
 
 logger.info(f"Generating bulk peaks from {snakemake.input.bam}")  # type: ignore
 logger.info("Using parameters %s:", dict(snakemake.params))  # type: ignore
 
-# read_groups = [
-#     lambda r: (r.is_read1 and r.is_forward and r.is_proper_pair) or (r.is_read2 and r.is_reverse and r.is_proper_pair),
-#     lambda r: (r.is_read1 and r.is_reverse and r.is_proper_pair) or (r.is_read2 and r.is_forward and r.is_proper_pair),
-# ]
-read_groups = [
-    lambda r: r.is_read1 and r.is_forward,
-    lambda r: r.is_read1 and r.is_reverse,
-]
-
 with AlignmentFile(snakemake.input.bam[0], "rb") as bam:  # type: ignore
-    for rg in read_groups:
-        sw = SlidingWindow(bam, read_filter=rg, mode="peaks", collect_features=False)
-        try:
-            sw.write_regions(
-                snakemake.output.pqt,  # type: ignore
-                **snakemake.params,  # type: ignore
-            )
-            logger.info("Done")
-        except:
-            # delete output file if error/interrupt
-            os.remove(snakemake.output.pqt)  # type: ignore
-            logger.error(f"Detected Error/Interuption, deleted {snakemake.output.pqt}")  # type: ignore
-            sys.exit(1)
+    sw = SlidingWindow(bam)
+    regions = [
+        r
+        for r in sw.make_regions(
+            mode="peaks", collect_features=True, **snakemake.params
+        )
+    ]
 
 # sort and convert to bed
-df = pq.read_table(snakemake.output.pqt).to_pandas()  # type: ignore
-df.sort_values(["Chromosome", "Start", "End"], inplace=True)
+df = pd.DataFrame(regions).sort_values(["Chromosome", "Start", "End"])
+df["width"] = df["End"] - df["Start"]
 df.to_parquet(snakemake.output.pqt)  # type: ignore
-df = df[["Chromosome", "Start", "End", "Strand"]]
+df = df[["Chromosome", "Start", "End", "n_reads", "width"]]
 pr.PyRanges(df).to_bed(snakemake.output.bed)  # type: ignore
