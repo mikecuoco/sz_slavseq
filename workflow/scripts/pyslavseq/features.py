31938503a6f83920277a5d13f3c43b94f080eb51
mikecuoco <mcuoco@ucsd.edu>
2024-01-05 09:32:57 -0800

    update tag_reads, add is_ref_read

--- workflow/scripts/pyslavseq/features.py
+++ workflow/scripts/pyslavseq/features.py
@@ -9,33 +9,17 @@ from collections import namedtuple
 from pysam import AlignedSegment
 import numpy as np
 
-
-def is_ref_read(read: AlignedSegment) -> bool:
-    "return True if read is ref, False if non-ref based on mate tag"
-
-    # get cigar at start of read, accounting for mate and orientation
-    if read.is_read1:
-        if not read.has_tag("MC"):
-            return False
-        cigar = re.findall(r"(\d+)([MIDNSHP=X])", str(read.get_tag("MC")))
-        end = cigar[-1] if not read.is_reverse else cigar[0]
-        clipped = int(end[0]) if end[1] == "H" or end[1] == "S" else 0
-        return read.is_proper_pair and (clipped < 30)
-    else:
-        cigar = re.findall(r"(\d+)([MIDNSHP=X])", read.cigarstring)
-        end = cigar[-1] if read.is_reverse else cigar[0]
-        clipped = int(end[0]) if end[1] == "H" or end[1] == "S" else 0
-        return clipped < 30
-
-
 # use named tuple to convert AlignedSegment
 # gives a speedup over using pysam.AlignedSegment (about 1.5x)
 Read = namedtuple(
     "Read",
     [
+        "query_name",
         "reference_name",
         "reference_start",
         "reference_end",
+        "five_end",
+        "three_end",
         "read_length",
         "is_read1",
         "is_read2",
@@ -49,6 +33,7 @@ Read = namedtuple(
         "cigarstring",
         "alignment_score",
         "L1_alignment_score",
+        "L1_mapping_quality",
         "L1_reference_start",
         "L1_reference_end",
         "L1_Acount",
@@ -68,9 +53,12 @@ Read = namedtuple(
 def read_to_namedtuple(read: AlignedSegment) -> Read:
     "convert pysam.AlignedSegment to hashable namedtuple Read"
     return Read(
+        read.query_name,
         read.reference_name,
         read.reference_start,
         read.reference_end,
+        read.reference_end if read.is_reverse else read.reference_start,
+        read.reference_start if read.is_reverse else read.reference_end,
         read.infer_read_length(),
         read.is_read1,
         read.is_read2,
@@ -84,6 +72,7 @@ def read_to_namedtuple(read: AlignedSegment) -> Read:
         read.cigarstring,
         read.get_tag("AS") if read.has_tag("AS") else None,
         read.get_tag("L1") if read.has_tag("L1") else None,
+        read.get_tag("LQ") if read.has_tag("LQ") else None,
         read.get_tag("LS") if read.has_tag("LS") else None,
         read.get_tag("LE") if read.has_tag("LE") else None,
         read.get_tag("LA") if read.has_tag("LA") else None,
@@ -94,7 +83,7 @@ def read_to_namedtuple(read: AlignedSegment) -> Read:
         read.next_reference_name,
         read.next_reference_start,
         read.is_proper_pair,
-        is_ref_read(read),
+        read.get_tag("RR"),
         read.is_duplicate,
     )
 
@@ -115,71 +104,14 @@ def gini(array):
     )  # Gini coefficient
 
 
-def basic_stats(p: dict):
-    """
-    Calculate basic stats about a region
-    :param p: dict of region, must contain "reads" key
-    """
-
-    for n in ["Chromosome", "Start", "End", "reads"]:
-        assert n in p, f"Region must contain {n}"
-
-    # iterate over reads to calculate stats
-    for mate in ["r1", "r2"]:
-        for k in ["n_ref_reads", "max_mapq", "n_duplicates", "n_fwd", "n_rev"]:
-            p[f"{mate}_{k}"] = 0
-        p[f"{mate}_starts"] = []
-
-    for r in p["reads"]:
-        mate = "r1" if r.is_read1 else "r2"
-        if r.is_duplicate:
-            p[f"{mate}_n_duplicates"] += 1
-            continue
-
-        if r.is_reverse:
-            p[f"{mate}_n_rev"] += 1
-        else:
-            p[f"{mate}_n_fwd"] += 1
-
-        p[f"{mate}_max_mapq"] = max(p[f"{mate}_max_mapq"], r.mapping_quality)
-        p[f"{mate}_n_ref_reads"] += r.is_ref_read
-
-        if r.is_forward:
-            p[f"{mate}_starts"].append(r.reference_start)
-        else:
-            p[f"{mate}_starts"].append(r.reference_end)
-
-    for mate in ["r1", "r2"]:
-        p[f"{mate}_n_unique_starts"] = len(set(p[f"{mate}_starts"]))
-        p[f"{mate}_n_reads"] = p[f"{mate}_n_fwd"] + p[f"{mate}_n_rev"]
-        del p[f"{mate}_starts"]
-    p["n_reads"] = p["r1_n_reads"] + p["r2_n_reads"]
-
-    # add strand info
-    if (p["r1_n_fwd"] == 0) and (p["r1_n_rev"] > 0):
-        p["Strand"] = "-"
-    elif (p["r1_n_rev"] == 0) and (p["r1_n_fwd"] > 0):
-        p["Strand"] = "+"
-    else:
-        p["Strand"] = "."
-
-    # remove reads
-    del p["reads"]
-
-    return p
-
-
 TAGS = [
     "alignment_score",
-    "alignment_score_normed",
     "L1_alignment_score",
-    "L1_alignment_score_normed",
+    "L1_mapping_quality",
     "L1_reference_start",
     "L1_reference_end",
     "L1_Acount",
     "mate_alignment_score",
-    "mate_alignment_score_normed",
-    "mate_read_length",
     "num_supp_alignments",
 ]
 
@@ -187,7 +119,7 @@ TAGS = [
 def features(p: dict) -> dict:
     """
     Extract features from a window of reads
-    All reads must be read1
+    All reads must be read2 or a contig
     :param p: dict of region, must contain "reads" key
     """
 
@@ -205,18 +137,17 @@ def features(p: dict) -> dict:
         "Start": p["Start"],
         "End": p["End"],
         "n_reads": 0,
+        "n_contigs": 0,
         "n_fwd": 0,
         "n_rev": 0,
         "n_duplicates": 0,
         "n_proper_pairs": 0,
         "n_ref_reads": 0,
-        "3end_gini": float(0),
-        "5end_gini": float(0),
         "max_mapq": 0,
-        "orientation_bias": float(0),
-        "frac_proper_pairs": float(0),
-        "frac_duplicates": float(0),
-        "n_unique_starts": 0,
+        "n_unique_5end": 0,
+        "n_unique_3end": 0,
+        "3end_gini": 0,
+        "5end_gini": 0,
     }
 
     for tag in TAGS:
@@ -228,8 +159,8 @@ def features(p: dict) -> dict:
     for i, r in enumerate(p["reads"]):
         if type(r) != Read:
             raise Exception("Reads must be of type Read")
-        if not r.is_read1:
-            raise Exception("Reads must all be read1")
+        if r.is_read2:
+            raise Exception("Reads must not be read2")
 
         if i == 0:
             start = r.reference_start
@@ -238,17 +169,12 @@ def features(p: dict) -> dict:
             f["n_duplicates"] += 1
             continue
 
-        l["starts"].append(r.reference_start)
-        if not r.is_reverse:
-            l["3end"].append(r.reference_end - start)
-            l["5end"].append(r.reference_start - start)
-        else:
-            l["3end"].append(r.reference_start - start)
-            l["5end"].append(r.reference_end - start)
-
+        l["3end"].append(r.three_end - start)
+        l["5end"].append(r.five_end - start)
         l["mapq"].append(r.mapping_quality)
         f["n_proper_pairs"] += r.is_proper_pair
         f["n_ref_reads"] += r.is_ref_read
+        f["n_contigs"] += not r.is_read1
 
         if r.is_reverse:
             f["n_rev"] += 1
@@ -256,22 +182,7 @@ def features(p: dict) -> dict:
             f["n_fwd"] += 1
 
         for tag in TAGS:
-            if "_normed" in tag:
-                if getattr(r, tag.replace("_normed", "")):
-                    if tag in [
-                        "L1_alignment_score_normed",
-                        "mate_alignment_score_normed",
-                    ]:  # adjust alignments scores for read length
-                        l[tag].append(
-                            getattr(r, tag.replace("_normed", ""))
-                            / getattr(r, "mate_read_length")
-                        )
-                    elif tag in ["alignment_score_normed"]:
-                        l[tag].append(
-                            getattr(r, tag.replace("_normed", ""))
-                            / getattr(r, "read_length")
-                        )
-            elif getattr(r, tag):
+            if getattr(r, tag):
                 l[tag].append(getattr(r, tag))
 
     # compute mean and quantiles for these features
@@ -279,18 +190,18 @@ def features(p: dict) -> dict:
         if len(l[tag]) > 0:
             quantiles = np.quantile(l[tag], [0, 0.25, 0.5, 0.75, 1])
             for n, q in zip([0, 0.25, 0.5, 0.75, 1], quantiles):
-                f[tag + "_q" + str(n)] = q
+                f[tag + "_q" + str(n)] = float(q)
             f[tag + "_mean"] = np.mean(l[tag])
-
-    f["n_reads"] = f["n_fwd"] + f["n_rev"]
 
     # if reads are empty, return empty features
     if f["n_reads"] == 0:
         return f
 
+    f["n_reads"] = f["n_fwd"] + f["n_rev"]
     f["3end_gini"] = gini(np.array(l["3end"], dtype=np.float64))
     f["5end_gini"] = gini(np.array(l["5end"], dtype=np.float64))
     f["max_mapq"] = max(l["mapq"])
-    f["n_unique_starts"] = len(set(l["starts"]))
+    f["n_unique_5end"] = len(set(l["5end"]))
+    f["n_unique_3end"] = len(set(l["3end"]))
 
     return f
--- workflow/scripts/tag_reads.py
+++ workflow/scripts/tag_reads.py
@@ -2,16 +2,51 @@
 # Created on: May 12, 2023 at 2:22:04 PM
 __author__ = "Michael Cuoco"
 
-import pysam, sys
+import pysam, sys, re, pdb
 
 sys.stderr = open(snakemake.log[0], "w")
 
 pysam.set_verbosity(0)
 
-# initialize variables
-r1, r2 = type("", (), {})(), type("", (), {})()
-r1.query_name, r2.query_name = "", None
-assert r1.query_name != r2.query_name
+
+def is_ref_read(read: pysam.AlignedSegment) -> bool:
+    "return True if read is ref, False if non-ref based on mate tag"
+
+    assert not read.is_read2, "Read cannot be read2"
+    # get cigar at start of read, accounting for mate and orientation
+    if read.is_read1:
+        if not read.has_tag("MC"):
+            return False
+        cigar = re.findall(r"(\d+)([MIDNSHP=X])", str(read.get_tag("MC")))
+        end = cigar[-1] if not read.is_reverse else cigar[0]
+        clipped = int(end[0]) if end[1] == "H" or end[1] == "S" else 0
+        return read.is_proper_pair and (clipped < 30)
+    else:
+        cigar = re.findall(r"(\d+)([MIDNSHP=X])", read.cigarstring)
+        end = cigar[0] if read.is_reverse else cigar[-1]
+        clipped = int(end[0]) if end[1] == "H" or end[1] == "S" else 0
+        return clipped < 30
+
+
+def reset_reads():
+    """Return empty r1 and r2"""
+    r1, r2 = type("", (), {})(), type("", (), {})()
+    r1.query_name, r2.query_name = "", None
+    return r1, r2
+
+
+r1, r2 = reset_reads()
+
+
+def next_primary(bam):
+    """Return the next primary alignment in bam"""
+    while True:
+        r = next(bam)
+        if r.is_secondary or r.is_supplementary:
+            continue
+        else:
+            return r
+
 
 # iterate over read IDs in line1_bam, match with genome_bam reads
 # save primary r1 from genome, primary r2 from genome, and primary r2 from line-1 to tab-delimited file
@@ -23,23 +58,27 @@ with pysam.AlignmentFile(snakemake.input["genome_bam"], "rb") as genome_bam:
             # iterate over alignments in line1_bam
             for r_l1 in line1_bam:
                 # skip secondary and supplementary alignments
-                if r_l1.is_secondary or r_l1.is_supplementary:
+                if (
+                    r_l1.is_secondary
+                    or r_l1.is_supplementary
+                    or r_l1.is_unmapped
+                    or r_l1.is_read1
+                ):
                     continue
 
                 # find matching reads in genome_bam
                 while (r1.query_name != r2.query_name) or (
                     r1.query_name != r_l1.query_name
                 ):
-                    r_genome = next(genome_bam)
-
-                    # get each read's primary alignment
-                    if r_genome.is_secondary or r_genome.is_supplementary:
-                        continue
+                    r_genome = next_primary(genome_bam)
 
                     # get the read, save as r1 or r2
                     if r_genome.is_read1:
                         r1 = r_genome
+                    elif r_genome.is_read2:
+                        r2 = r_genome
                     else:
+                        r1 = r_genome
                         r2 = r_genome
 
                 # skip if read1 is unmapped
@@ -49,13 +88,13 @@ with pysam.AlignmentFile(snakemake.input["genome_bam"], "rb") as genome_bam:
                 assert (
                     r1.query_name == r2.query_name == r_l1.query_name
                 ), "Read IDs do not match"
-
-                # add tags to read1
+                r1.set_tag("RR", int(is_ref_read(r1)))
+                r1.set_tag("LQ", r_l1.mapping_quality)  # L1 mapping quality
                 r1.set_tag("L1", r_l1.get_tag("AS"))  # L1 alignment score
-                r1.set_tag("MS", r2.get_tag("AS"))  # mate alignment score
                 r1.set_tag("LS", r_l1.reference_start)  # L1 start
                 r1.set_tag("LE", r_l1.reference_end)  # L1 end
                 r1.set_tag("LA", r_l1.query_sequence.count("A"))  # L1 A count
+                r1.set_tag("MS", r2.get_tag("AS"))  # mate alignment score
                 r1.set_tag(
                     "ML",
                     r2.infer_read_length()
@@ -65,6 +104,7 @@ with pysam.AlignmentFile(snakemake.input["genome_bam"], "rb") as genome_bam:
 
                 # write reads to output bam
                 out_bam.write(r1)
-                out_bam.write(r2)
+                if r1.is_read1:
+                    out_bam.write(r2)
 
 sys.stderr.close()
